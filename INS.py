e088e17a14ab2153bd41e861c65bf19c8dd3a1ee
Sophia-CO <45643025+Sophia-CO@users.noreply.github.com>
2019-01-10 10:08:26 +0100

    Add files via upload

---
+++ INS.py
@@ -0,0 +1,21 @@
+def ins(n,A):
+    num = 0
+    for i in range(1, len(a)):
+        k = i
+        while k > 0 and a[k] < a[k - 1]:
+            a[k - 1], a[k] = a[k], a[k - 1]
+            num += 1
+            k -= 1
+    return num
+with open('INSERT DIRECTORY\rosalind_ins (1).txt') as f:
+    i=0
+    n=0
+    a=[]
+    for line in f:
+        text=line.strip()
+        if i==0:
+            n=int(text)
+        else:
+            a=[int(t) for t in text.split(' ')]
+        i+=1
+    print (ins(n,a))
